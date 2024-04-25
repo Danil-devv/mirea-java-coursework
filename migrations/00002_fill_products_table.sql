1f852adf258ca7134951922ec922d1f166557b17
danil <s3menovd4nil@yandex.ru>
2024-04-25 20:40:14 +0300

    fix: migrations

---
+++ migrations/00002_fill_products_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO products(id, image, name, price)
+VALUES (1, './image/hammer_1.png', 'Hammer', 15),
+       (2, './image/shovel_1.png', 'Shovel', 27),
+       (3, './image/hoe_1.png', 'The Hoe', 20),
+       (4, './image/rake_1.png', 'Rake', 30);
